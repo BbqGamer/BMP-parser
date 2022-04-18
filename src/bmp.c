1ffe0b4b0d30fc920c41a05eb2e25e9af13e4a38
adam <korba.adam@gmail.com>
2022-04-18 14:55:05 +0200

    Change argv reading

--- src/bmp.c
+++ src/bmp.c
@@ -4,7 +4,15 @@
 
 int main(int argc, char* argv[]) {
     if(argc == 2) {
-        return histProgram(argv[1]);
+        char c;
+        printf("0. Show file stats\n1. Decode steganography\n");
+        scanf("%c", &c);
+        if(c == '0') {
+            return histProgram(argv[1]);
+        } else if (c == '1') {
+            printf("DECODE!\n");
+        }
+        return 0;
     } else if (argc == 3) {
         return grayscaleProgram(argv[1], argv[2]);
     } else if (argc == 4) {
