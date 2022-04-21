c60a0990acf570677dd5b2d3508d90be88b29d5e
adam <korba.adam@gmail.com>
2022-04-21 13:59:47 +0200

    Support encoding message

--- src/steganography.c
+++ src/steganography.c
@@ -1,78 +1,59 @@
 #include "steganography.h"
 
-int decodeSteganography(char *file)
-{
+int decodeSteganography(char* file) {
     printf("READ");
     return 0;
 }
 
-int encodeSteganography(char *inFilename, char *outFilename, char *message)
-{
-    //     FILE* in = fopen(inFilename, "rb");
-    //     if(in == NULL) {
-    //         printf("Error opening input file!");
-    //         return FILE_NOT_FOUND;
-    //     }
+int encodeSteganography(char* inFilename, char* outFilename, char* message) {
+    FILE *in = fopen(inFilename, "rb");
+    if (in == NULL)
+    {
+        printf("Error opening input file!");
+        return FILE_NOT_FOUND;
+    }
 
-    //     FILE* out = fopen(outFilename, "wb");
-    //     if(out == NULL) {
-    //         printf("Error opening output file!");
-    //         fclose(in);
-    //         return FILE_NOT_FOUND;
-    //     }
+    FILE *out = fopen(outFilename, "wb");
+    if (out == NULL)
+    {
+        printf("Error opening output file!");
+        fclose(in);
+        return FILE_NOT_FOUND;
+    }
 
-    //     BITMAPFILEHEADER header;
-    //     readFileHeader(in, &header);
+    // READ BITMAP
+    BMP *bitmap = (BMP *)malloc(sizeof(BMP));
+    readBitmap(in, bitmap);
 
-    //     BITMAPINFOHEADER infoHeader;
-    //     readInfoHeader(in, &infoHeader);
+    encodeMessage(bitmap, message);
 
-    //     // writeFileHeader(out, header);
-    //     // writeInfoHeader(out, infoHeader);
+    writeBitmap(out, bitmap);
 
-    //     //encodeMessage(in, out, message, infoHeader);
-
-    //     fclose(in);
-    //     fclose(out);
-
-    //     return 0;
-    // }
-
-    // void encodeMessage(FILE* in, FILE* out, char* message, LPBITMAPINFOHEADER infoHeader) {
-    //     int l = strlen(message);
-    //     int c = 0;
-
-    //     int lineSize = ((infoHeader->biBitCount * infoHeader->biWidth + 31) / 32) * 4;
-    //     int sum;
-
-    //     BYTE* buffer = (BYTE*)malloc(sizeof(BYTE)*lineSize);
-    //     for(int i = 0; i < infoHeader->biHeight; i++) {
-    //         fread(buffer, sizeof(BYTE), lineSize, in);
-
-    //         for(int i = 0; i < infoHeader->biWidth; i+=8) {
-    //             if(c < l) {
-    //                 buffer[i] = 100;
-    //                 //encodeChar(buffer, i, message[c]);
-    //             }
-    //             c++;
-    //         }
-
-    //         fwrite(buffer, sizeof(BYTE), lineSize, out);
-    //     }
-    //     free(buffer);
+    // CLEANUP
+    freeBitmap(bitmap);
+    fclose(in);
+    fclose(out);
     return 0;
 }
 
-void encodeChar(BYTE *buffer, int index, char c)
-{
+void encodeMessage(BMP* bitmap, char* message) {
+    int l = strlen(message);
+    int c = 0;
+
+    encodeChar(bitmap->pixels, 0, l);
+    for(int i = 0; i < l; i++) {
+        encodeChar(bitmap->pixels, (i+1)*8, message[i]);
+    }
+}
+
+void encodeChar(BYTE* buffer, int index, char c) {
     int mask = 0xFE;
     char bit;
 
-    for (int i = 0; i < 8; i++)
-    {
+    for(int i = 0; i < 8; i++) {
         bit = c & 1;
 
-        buffer[index + i] = (buffer[index + i] & mask) + bit;
+        buffer[index+i] = (buffer[index+i] & mask) + bit;
 
         c = c >> 1;
     }
--- src/steganography.h
+++ src/steganography.h
@@ -6,7 +6,7 @@
 int decodeSteganography(char *file);
 
 int encodeSteganography(char *inFilename, char *outFilename, char *message);
-void encodeMessage(FILE *in, FILE *out, char *message, LPBITMAPINFOHEADER infoHeader);
+void encodeMessage(BMP* bitmap, char* message);
 void encodeChar(BYTE *buffer, int index, char c);
 
 #endif
